fa23666814927cba4a96ae2eff9d6e89d5e516fb
SantiagoMoralesDevS <santimorales456@gmail.com>
2026-09-15 04:23:14 -0500

    HU-05: formulario de creacion de vivienda con generacion de codigo de invitacion y conexion a Firestore

--- our-house/src/App.tsx
+++ our-house/src/App.tsx
@@ -1,3 +1,4 @@
+import CrearViviendaForm from './components/CrearViviendaForm'
 import { useState } from 'react'
 import heroImg from './assets/hero.png'
 import reactLogo from './assets/react.svg'
@@ -29,6 +30,8 @@ function App() {
           Count is {count}
         </button>
       </section>
+
+      <CrearViviendaForm />
 
       <div className="ticks"></div>
 
